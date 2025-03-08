fc4974c50edb43afb022ca5c5912bd0b860cf206
mjtranter <mjtranter2003@outlook.com>
2025-03-08 00:24:59 +0000

    Test country code

--- my-react-app/api/fetchCountryCode.js
+++ my-react-app/api/fetchCountryCode.js
@@ -1,8 +1,9 @@
 export default async function handler(req, res) {
     try {
+        const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
+        console.log(ip);
         const response = await fetch("http://ip-api.com/json/?fields=countryCode")
         const data = await response.json();
-        console.log(data);
         res.status(200).json(data);
     }
     catch (error) {
