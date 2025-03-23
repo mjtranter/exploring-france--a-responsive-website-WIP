105b19a61d8bc03384633db77cdae2b56327ca40
mjtranter <mjtranter2003@outlook.com>
2025-03-23 23:47:20 +0000

    More events and filter system added

--- my-react-app/src/pages/music/events/events.js
+++ my-react-app/src/pages/music/events/events.js
@@ -1,17 +1,23 @@
 import { useTranslation } from "react-i18next";
 import EventCalendar from "../../../components/eventCalendar/eventCalendar";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { CountryCodeContext } from "../../../App";
 import Popup from "../../../components/popup/popup";
 import Button from "../../../components/button/button";
 import MonteCarloSpringArts from "../../../assets/images/monte-carlo-spring-arts.jpg";
+import BanlieuesBleues from '../../../assets/images/banlieues-bleues.jpg';
 import ParisJazz from '../../../assets/images/paris-jazz.jpg';
+import CognacBluesPassions from '../../../assets/images/cognac-blues-passions.jpg';
+import Beatles from '../../../assets/images/beatles.jpg';
 import './events.css';
 
-const images = [MonteCarloSpringArts, ParisJazz];
+const images = [MonteCarloSpringArts, BanlieuesBleues, ParisJazz, CognacBluesPassions, Beatles];
 
 export default function Events() {
     const ns = "music";
     const { t } = useTranslation(ns);
+
+    const countryCode = useContext(CountryCodeContext);
 
     const [popupData, setPopupData] = useState({type: "", title: "", content: null, ns: ns, visible: false});
     
@@ -25,6 +31,12 @@ export default function Events() {
 
     const events = t('list-events', { returnObjects: true });
 
+    const [filterSelected, setFilterSelected] = useState("All");
+
+    const changeFilter = (filter) => {
+        setFilterSelected(filter);
+    }
+
     return (
         <div className="category-component">
             <div className={"overlay" + (popupData.visible ? " visible" : "")}></div>
@@ -32,7 +44,7 @@ export default function Events() {
                 <Popup {...popupData} hidePopup={hidePopup} />
             </div>
             <div className="whats-on">
-                <div className="header-row">
+                <div className="whats-on-header-row">
                     <h5><b>{t('whats-on')}</b></h5>
                     <Button type="default" text={t('view-full-calendar')} onClick={() => showPopup("default", 'full-calendar', "calendar" )} />
                 </div>
@@ -40,16 +52,32 @@ export default function Events() {
                 <EventCalendar ns={ns} type="short" />
             </div>
             
-            <h5 className="category-heading"><b>{t('featured-events')}</b></h5>
+            <div className="events-header-row">
+                <h5 className="featured-events"><b>{t('featured-events')}</b></h5>
+                <div className="events-filter">
+                    <Button type={filterSelected === "All" ? "filter selected" : "filter"} text={t('all')} onClick={() => changeFilter("All")} />
+                    <Button type={filterSelected === "Local" ? "filter selected" : "filter"} text={t('local')} onClick={() => changeFilter("Local")} />
+                    <Button type={filterSelected === "Free" ? "filter selected" : "filter"} text={t('free')} onClick={() => changeFilter("Free")} />
+                    <Button type={filterSelected === "Connections" ? "filter-connection selected" : "filter-connection"} text={t('connections')} onClick={() => changeFilter("Connections")} />
+                </div>
+            </div>
+            
 
 
-            {events.map(event => (
+            {events.filter(event => {
+                if (filterSelected === "All") return true;
+                if (filterSelected === "Local" && event.local === "True") return true;
+                if (filterSelected === "Free" && event.free === "True") return true;
+                if (filterSelected === "Connections" && event.connection === countryCode) return true;
+                return false;
+            })
+            .map(event => (
                 <div key={event.id} className="event-frame">
                     <img className="event-icon" src={images[event.id - 1]} alt={event.text} />
                     <div className="event-content">
                         <div className="event-title">
                             <b>{event.text}</b>
-                            <i>{event.longStart}{t('time-period')}{event.longEnd}</i>
+                            <i className="event-date">{event.longDate}</i>
                         </div>
 
                         <div className="event-details">
