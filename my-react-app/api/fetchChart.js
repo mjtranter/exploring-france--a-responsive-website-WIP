1af1e5e22e177f4ae04e83d76056b95b878ffa2c
mjtranter <mjtranter2003@outlook.com>
2025-03-25 17:43:08 +0000

    Test response

--- my-react-app/api/fetchChart.js
+++ my-react-app/api/fetchChart.js
@@ -1,10 +1,10 @@
 export default async function handler(req, res) {
     try {
         const api = process.env.LAST_FM_API_KEY;
-        console.log(api);
+
         const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=france&api_key=${api}&format=json&limit=10`);
         const data = await response.json();
-
+        console.log(data);
         res.status(200).json(data);
     }
     catch (error) {
