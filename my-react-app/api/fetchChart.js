4bb9b8674d2c2f378651f5061288fcf2a6ed9408
mjtranter <mjtranter2003@outlook.com>
2025-03-25 21:18:25 +0000

    Test response

--- my-react-app/api/fetchChart.js
+++ my-react-app/api/fetchChart.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     try {
         const response = await fetch(`https://api.deezer.com/playlist/1109890291/tracks`);
         const data = await response.json();
-        res.status(200).json(data.data);
+        res.status(200).json(data.data.slice(0, 10));
     }
     catch (error) {
         res.status(500).json({ error: "Failed to fetch top 10" });
