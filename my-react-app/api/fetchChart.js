a40f192d497d29ffd228ecb85ef991d68e02258b
mjtranter <mjtranter2003@outlook.com>
2025-03-25 20:55:04 +0000

    Update api

--- my-react-app/api/fetchChart.js
+++ my-react-app/api/fetchChart.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-        const response = await fetch(`https://api.deezer.com/chart/FR/tracks`);
+        const response = await fetch(`https://api.deezer.com/playlist/1109890291/tracks`);
         const data = await response.json();
         res.status(200).json(data);
     }
