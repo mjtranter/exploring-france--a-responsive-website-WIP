2c2bce16b38de668e2749dd6d597eb2ba7032511
mjtranter <mjtranter2003@outlook.com>
2025-03-31 19:23:33 +0100

    Timeline added (incpmplete)

--- my-react-app/src/pages/film/history/history.js
+++ my-react-app/src/pages/film/history/history.js
@@ -1,7 +1,87 @@
-export default function History({ns}) {
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import '../../../assets/colours/colours.css';
+import './history.css';
+import { HiOutlineBookOpen } from 'react-icons/hi';
+import { PiFilmSlate } from 'react-icons/pi';
+import { GoPerson }  from 'react-icons/go';
+import UKIcon from '../../../assets/images/uk-icon.png';
+import QuebecIcon from '../../../assets/images/quebec-icon.png';
+import JapanIcon from '../../../assets/images/japan-icon.png';
+import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { CountryCodeContext } from '../../../App';
+
+export default function History() {
+    const ns = "film";
+    const { t } = useTranslation(ns);
+
+    let countryCode = useContext(CountryCodeContext);
+    const countryParam = new URLSearchParams(window.location.search).get("countrySim");
+    if (countryParam) countryCode = countryParam.toLowerCase();
+
+    let timelineItems = t('timeline', { returnObjects: true });
+
     return (
         <div>
-            
+            <VerticalTimeline layout="1-column-left" lineColor="var(--dark-theme)">
+                {timelineItems.filter(timelineItem => {
+                    if (timelineItem.type === "film") return true;
+                    if (timelineItem.type === "person") return true;
+                    if (timelineItem.type.substring(0,10) === "connection" && timelineItem.type.substring(11,13) === countryCode) return true;
+                    if (timelineItem.type === "information") return true;
+                    return false;
+                })      
+                .map(timelineItem => {
+                    let primaryColour;
+                    let iconName;
+                    let itemClass;
+                    if (timelineItem.type === "film") {
+                        primaryColour = "var(--teal-theme)";
+                        iconName = <PiFilmSlate />;
+                        itemClass = "film-item";
+                    }
+                    
+                    else if (timelineItem.type === "person") {
+                        primaryColour = "var(--purple-theme)";
+                        iconName = <GoPerson />;
+                        itemClass = "person-item";
+                    }
+
+                    else if (timelineItem.type === "connection " + countryCode) {
+                        primaryColour = "var(--french-red)";
+                        itemClass = "connection-item";
+
+                        switch (countryCode) {
+                            case "jp":
+                                iconName = <img src={JapanIcon} alt="Japan Connection" style={{ width: "100%", height: "100%", borderRadius: "50%" }} />
+                                break;
+                            case "ca":
+                                iconName = <img src={QuebecIcon} alt="Quebec Connection" style={{ width: "100%", height: "100%", borderRadius: "50%" }} />
+                                break;
+                            default:
+                                iconName = <img src={UKIcon} alt="UK Connection" style={{ width: "100%", height: "100%", borderRadius: "50%" }} />
+                                break;
+                        }
+                    }
+
+                    else {
+                        primaryColour = "var(--orange-theme)";
+                        iconName = <HiOutlineBookOpen />
+                        itemClass = "information-item"; 
+                    }
+
+                    return (
+                        <VerticalTimelineElement key={timelineItem.title} className={"vertical-timeline-element " + itemClass} iconStyle={{ background: primaryColour, color: "var(--french-white)" }} icon={iconName}>
+                            <div className="text-row">
+                                <h5 className="vertical-timeline-element-title"><b>{timelineItem.title}</b></h5>
+                                <p className="vertical-timeline-date"><i>{timelineItem.date}</i></p>
+                            </div>
+                            <div className="timeline-content" dangerouslySetInnerHTML={{__html: timelineItem.content}} />
+                        </VerticalTimelineElement>
+                    )
+                })}
+            </VerticalTimeline>
         </div>
     )
 }
--- my-react-app/src/pages/music/overview/overview.js
+++ my-react-app/src/pages/music/overview/overview.js
@@ -9,22 +9,22 @@ export default function Overview() {
 
     return (
         <div className="category-component">
-            <div className="overview-image-container right">
-                <img className="overview-image" src={ChristineAndTheQueens} alt="Christine and the Queens" />
+            <div className="description-image-container right">
+                <img className="description-image" src={ChristineAndTheQueens} alt="Christine and the Queens" />
                 <div dangerouslySetInnerHTML={{__html: t('christine-and-the-queens')}} />
             </div>    
             <p className="category-description">{t('overview-description-1')}</p>
 
             <h6 className="category-heading"><b>{t('overview-events')}</b></h6>
-            <div className="overview-image-container left fete">
-                <img className="overview-image" src={FeteDeLaMusique} alt="Fête de la Musique" />         
+            <div className="description-image-container left fete">
+                <img className="description-image" src={FeteDeLaMusique} alt="Fête de la Musique" />         
                 <div dangerouslySetInnerHTML={{__html: t('fete-de-la-musique')}} />
             </div>
             <p className="category-description">{t('overview-description-2')}</p>
             
             <h6 className="category-heading"><b>{t('overview-global')}</b></h6>
             <div className="category-description" dangerouslySetInnerHTML={{__html: t('overview-description-3')}} />
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/-XyLecY2JyE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <iframe className="music-iframe" width="100%" height="370" src="https://www.youtube.com/embed/-XyLecY2JyE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     )
 }
