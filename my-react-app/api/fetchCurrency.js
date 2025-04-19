3eca08c3cbd66806af32de3fd2229ea21e143fcf
mjtranter <mjtranter2003@outlook.com>
2025-04-19 15:08:59 +0100

    Test conversion rates

--- my-react-app/api/fetchCurrency.js
+++ my-react-app/api/fetchCurrency.js
@@ -17,6 +17,6 @@ export default async function handler(req, res) {
         res.status(200).json(data);
     }
     catch (error) {
-        res.status(500).json({ error: "Failed to fetch weather" });
+        res.status(500).json({ error: "Failed to fetch conversion rate" });
     }
 }
