640d110a1936e5105689b934adb5eca5d1d13dbf
mjtranter <mjtranter2003@outlook.com>
2025-05-02 02:18:43 +0100

    Test fetch country code

--- my-react-app/api/fetchCountryCode.js
+++ my-react-app/api/fetchCountryCode.js
@@ -1,9 +1,10 @@
 export default async function handler(req, res) {
     try {
-        const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
-        const response = await fetch(`http://ip-api.com/json/${ip}?fields=countryCode`);
+        //const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
+        const ip = req.headers["x-vercel-ip-country"];
+        //const response = await fetch(`http://ip-api.com/json/${ip}?fields=countryCode`);
         const data = await response.json();
-        res.status(200).json(data);
+        res.status(200).json({ ip });
     }
     catch (error) {
         res.status(500).json({ error: "Failed to fetch country code" });
