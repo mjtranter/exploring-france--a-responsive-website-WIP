b7b8250ca438fce99797aa7976e015e0eb402962
mjtranter <mjtranter2003@outlook.com>
2025-05-02 02:57:03 +0100

    Update fetch country code

--- my-react-app/api/fetchCountryCode.js
+++ my-react-app/api/fetchCountryCode.js
@@ -1,9 +1,6 @@
 export default async function handler(req, res) {
     try {
-        //const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
         const countryCode = req.headers["x-vercel-ip-country"];
-        //const response = await fetch(`http://ip-api.com/json/${ip}?fields=countryCode`);
-        //const data = await response.json();
         res.status(200).json(countryCode);
     }
     catch (error) {
